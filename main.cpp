a011f9954ed307bca15ec93729fd27491e8663a9
jonmunoz7 <j.munoz@opendeusto.es>
2023-11-14 11:23:59 +0100

    terter

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main() {
 
   timer.reset();
   timer.start();
-  resultado = calcularRMS(datos, longitudTrama); //hoa
+  resultado = calcularRMS(datos, longitudTrama); //hoagfdfgvf
   timer.stop();
   printf("\n****El valor Vrms es %f calculado en %lld us ****\n\n", resultado,
          timer.elapsed_time().count());
